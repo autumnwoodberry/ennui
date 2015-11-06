75a43ac323a0d585c583c17b4e5752a31d1d8114
Adam Woodbury <adamwoodbury@gmail.com>
2015-11-06 07:22:51 -0800

    Add a new photos app.

--- ennui/settings.py
+++ ennui/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'colorful',
     'shared',
+    'photos',
 )
 
 MIDDLEWARE_CLASSES = (
--- ennui/urls.py
+++ ennui/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', include('colorful.urls')),
+    url(r'^photos/', include('photos.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
